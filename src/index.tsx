bbb43dc7e8db105affba8ac5e5f2d069fc5e73b5
natehains <nathan.hains@modelb.com>
2021-08-22 22:09:47 -0400

    Implementing AuthProvider component.

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AuthProvider from './auth/AuthProvider';
+import { Router } from '@material-ui/icons';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
